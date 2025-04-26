5d58bff2f786efedb56555562129cdd0a734db70
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-26 23:06:40 +0530

    Home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { MainComponent } from "./components/main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, HeroComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
