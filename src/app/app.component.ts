64dc7da5a89cd2f20dea8966c719e3f77acca03e
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-26 22:39:03 +0530

    add foot compont and update home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mos_burgers_frontend';
+  title = 'MOS Burgers';
 }
